6b217372241b1c98657d4dab9f1b66b4cc46a267
Yeasin <yeasinhossen7972@gmail.com>
2022-08-28 00:19:20 +0600

    implemant login system

---
+++ src/config.js
@@ -0,0 +1,1 @@
+export const BASE_URL = 'https://api.shazlanihtisham.com/api/v1/auth';
---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,38 @@
+import axios from 'axios';
+import React, { useContext, useEffect, useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import Spinner from 'react-native-loading-spinner-overlay';
+import { AuthContext } from '../context/AuthContext';
+
+const HomeScreen = () => {
+    const { isLoading, logout } = useContext(AuthContext);
+    const [projects, setProjects] = useState([]);
+
+    useEffect(() => {
+        axios.get("https://api.shazlanihtisham.com/api/v1/projects")
+            .then(data => setProjects(data))
+    }, [])
+
+    return (
+        <View style={styles.container}>
+            <Spinner visible={isLoading} />
+            <Text style={styles.welcome}>Welcome to Homepage</Text>
+            {projects.map(project => <Text>{project.client}</Text>)}
+            <Button title="Logout" color="red" onPress={logout} />
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    welcome: {
+        fontSize: 18,
+        marginBottom: 8,
+    },
+});
+
+export default HomeScreen;
